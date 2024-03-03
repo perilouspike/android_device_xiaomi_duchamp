6cccf0b3f13b1c51d8cee320092eb4d2f65fdb75
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2024-03-03 11:10:21 +0530

    Fix

--- device.mk
+++ device.mk
@@ -69,13 +69,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery \
-    android.hardware.boot@1.1-impl.recovery \
-    android.hardware.boot@1.1-service \
-    android.hardware.boot@1.1-impl \
-    android.hardware.boot@1.0-impl \
-	android.hardware.boot@1.0-impl.recovery \
-	android.hardware.boot@1.0-service
+    android.hardware.boot@1.2-mtkimpl.recovery
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service    
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6897
@@ -98,9 +97,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
-    android.hardware.health@2.0-service \
-    android.hardware.health@2.0-impl.recovery \
-    android.hardware.health@2.0-service.rc
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service.rc
 
 # Mtk plpath utils
 PRODUCT_PACKAGES += \
