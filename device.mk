391e588359ae645ee9e3268ccb8ff91df6189150
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2024-03-11 18:02:15 +0530

    Duchamp : Global testing.

--- device.mk
+++ device.mk
@@ -7,59 +7,30 @@
 
 LOCAL_PATH := device/xiaomi/duchamp
 
-# Hidl Service
-PRODUCT_ENFORCE_VINTF_MANIFEST := true
-
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# API
-PRODUCT_SHIPPING_API_LEVEL := 31
-PRODUCT_TARGET_VNDK_VERSION := 34
 
 # A/B
 AB_OTA_UPDATER := true
 ENABLE_VIRTUAL_AB := true
-TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
 AB_OTA_PARTITIONS += \
-    apusys \
-    audio_dsp \
     boot \
-    ccu \
-    dpm \
     dtbo \
-    gpueb \
-    gz \
-    lk \
-    logo \
-    mcf_ota \
-    mcupm \
-    md1img \
-    mvpu_algo \
+    system \
+    product \
+    vendor \
     odm \
     odm_dlkm \
-    pi_img \
-    preloader_raw \
-    product \
-    scp \
-    spmfw \
-    sspm \
-    system \
-    system_ext \
-    tee \
     vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
-    vcp \
-    vendor \
     vendor_boot \
     vendor_dlkm \
-    mi_ext
+    vbmeta_system \
+    vbmeta_vendor
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
@@ -67,11 +38,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
-
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service    
 
 PRODUCT_PACKAGES := \
     bootctrl.mt6897 \
@@ -91,47 +57,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service.rc
 
 # Mtk plpath utils
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1
-
-# Keystore2
-PRODUCT_PACKAGES += \
-    android.system.keystore2
- 
-# Additional Target Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.vibrator-V1-ndk_platform \
-    libion \
-    libxml2 \
-    android.hardware.health@2.0-impl-default \
-    android.hardware.boot@1.0 \
-    android.hardware.keymaster@4.1 \
-    android.hardware.graphics.common@1.0
-
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.health@2.0-impl-default.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.graphics.common@1.0.so
-
-# Drm
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4
-
-# Keymint
-PRODUCT_PACKAGES += \
-    android.hardware.security.keymint \
-    android.hardware.security.secureclock \
-    android.hardware.security.sharedsecret    
