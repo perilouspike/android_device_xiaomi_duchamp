981ea65c2e74112f72cee206019106e7bf0d16b8
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2024-03-13 16:26:07 +0530

    Duchamp : Global testing #Attempt 1
    • Trying to boot it first without having decryption enabled.
    • The build was not able to boot on previous commit. So replacing the dtb.img file with a new one and made some changes to the make files.

--- twrp_duchamp.mk
+++ twrp_duchamp.mk
@@ -17,8 +17,7 @@ $(call inherit-product, device/xiaomi/duchamp/device.mk)
 
 # Configure launch_with_vendor_ramdisk.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
