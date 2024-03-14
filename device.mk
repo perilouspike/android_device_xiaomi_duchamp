2f81f5df576c1c075ec33c2e61fc7af288301dd4
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2024-03-15 00:38:26 +0530

    Duchamp: `GLOBAL` Setup for decryption #Attempt 1
    • Add necessary blobs related to decryption.
    • Made changes to the makefiles 
       - Update API and VNDK versions.
           * Originally, API level should be 34, but since current manifest's 
              (twrp-12.1) System SDK only supports at 32--tops, had to trim 
              down to 32.
           * VNDK version can be set as 34 though.
       - Made minor changes to the BC
       - Add `mtk_plpath_utils` from dump instead of building it from source
       
       Hope this time it'll decrypt!

--- device.mk
+++ device.mk
@@ -10,6 +10,12 @@ LOCAL_PATH := device/xiaomi/duchamp
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 34
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 32
+
 # A/B
 AB_OTA_UPDATER := true
 ENABLE_VIRTUAL_AB := true
@@ -58,7 +64,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
 
-# Mtk plpath utils
-PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so    
